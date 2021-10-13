97d53b045f3037966c544b753f729e6a5a67cc7e
andrei <git1@email.com>
2021-10-13 23:14:02 +0200

    created DynamicInputFields page

---
+++ src/pages/DynamicInputFields.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DynamicInputFields() {
+    return (
+        <div>
+            Dynamic Input Fields page
+        </div>
+    )
+}
+
+export default DynamicInputFields
