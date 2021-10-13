f24f02dd40854a0fd1a93f092af9aaa080e15c80
andrei <git1@email.com>
2021-10-13 23:14:19 +0200

    adding link
    adding link to the Dynamic Input Fields page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import DynamicInputFields from "./pages/DynamicInputFields";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Link to="/dynamicInputFields">Dynamic Input Fields</Link>
+              }
+            />
+            <Route path="/dynamicInputFields" element={<DynamicInputFields />} />
+          </Routes>
+        </Router>
       </header>
     </div>
   );
