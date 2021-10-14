f03c80ac466d0d3f1ea3721bb4710f65dfbf68c5
andrei <git1@email.com>
2021-10-14 15:58:25 +0200

    Update DynamicInputFields.js

--- src/pages/DynamicInputFields.js
+++ src/pages/DynamicInputFields.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Dynamic from '../components/Dynamic'
 
 function DynamicInputFields() {
     return (
         <div>
             Dynamic Input Fields page
+            <Dynamic />
         </div>
     )
 }
