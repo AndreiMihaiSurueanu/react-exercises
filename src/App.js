d87bdb1e318d5b72111e742187811f64faa520f8
andrei <git1@email.com>
2021-10-14 23:10:44 +0200

    added Home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import DynamicInputFields from "./pages/DynamicInputFields";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <Router>
           <Routes>
-            <Route
-              path="/"
-              element={
-                <Link to="/dynamicInputFields">Dynamic Input Fields</Link>
-              }
-            />
+            <header className="App-header">
+              <Route path="/" element={<Home />} />
+            </header>
             <Route path="/dynamicInputFields" element={<DynamicInputFields />} />
           </Routes>
         </Router>
-      </header>
     </div>
   );
 }
